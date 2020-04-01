c4cce74ae4708b2a901b4ce718377e0269cdd3df
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-04-01 20:47:49 +0700

    part 5 done

---
+++ app/src/main/java/com/gzeinnumer/chatapp/adapter/UserAdapter.java
@@ -0,0 +1,63 @@
+package com.gzeinnumer.chatapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.gzeinnumer.chatapp.R;
+import com.gzeinnumer.chatapp.databinding.UserItemBinding;
+import com.gzeinnumer.chatapp.model.User;
+
+import java.util.List;
+
+import javax.microedition.khronos.opengles.GL;
+
+//todo 39
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyHolder> {
+    private Context context;
+    private List<User> list;
+
+    public UserAdapter(List<User> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        UserItemBinding binding =  UserItemBinding.inflate(LayoutInflater.from(parent.getContext()));
+        context = parent.getContext();
+        return new MyHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        holder.bind(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        UserItemBinding binding;
+        MyHolder(@NonNull UserItemBinding itemView) {
+            super(itemView.getRoot());
+            this.binding = itemView;
+        }
+
+        void bind(User user) {
+            binding.username.setText(user.getUsername());
+            if(user.getImageURL().equals("default")){
+                binding.profileImage.setImageResource(R.mipmap.ic_launcher);
+            } else {
+                Glide.with(context).load(user.getImageURL()).into(binding.profileImage);
+            }
+        }
+    }
+}
--- app/src/main/java/com/gzeinnumer/chatapp/fragment/UsersFragment.java
+++ app/src/main/java/com/gzeinnumer/chatapp/fragment/UsersFragment.java
@@ -2,21 +2,80 @@ package com.gzeinnumer.chatapp.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.gzeinnumer.chatapp.R;
+import com.gzeinnumer.chatapp.adapter.UserAdapter;
+import com.gzeinnumer.chatapp.databinding.FragmentUsersBinding;
+import com.gzeinnumer.chatapp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class UsersFragment extends Fragment {
 
-
+    //todo 40
+    private FragmentUsersBinding binding;
+    private UserAdapter myAdapter;
+    private List<User> list = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        //todo 41
+        binding = FragmentUsersBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
+    }
+
+    //todo 42
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        readData();
+    }
+
+    //todo 43
+    private void readData() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users_chat_app");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    if(!Objects.equals(user.getId(), firebaseUser.getUid())){
+                        list.add(user);
+                    }
+                }
+                Log.d("MyZein", String.valueOf(list.size()));
+
+                myAdapter = new UserAdapter(list);
+                binding.rvData.setAdapter(myAdapter);
+                binding.rvData.setLayoutManager(new LinearLayoutManager(getContext()));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
