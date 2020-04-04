0926dc8b0aab34288e82d466b26cf6d56f1d2c78
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-04-04 17:57:30 +0700

    part 11 done

--- app/src/main/java/com/gzeinnumer/chatapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/gzeinnumer/chatapp/fragment/ProfileFragment.java
@@ -1,5 +1,10 @@
 package com.gzeinnumer.chatapp.fragment;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,8 +14,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,17 +29,32 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.gzeinnumer.chatapp.R;
 import com.gzeinnumer.chatapp.databinding.FragmentProfileBinding;
 import com.gzeinnumer.chatapp.model.User;
+
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_OK;
 
 //todo 65 part 10
 public class ProfileFragment extends Fragment {
 
     //todo 67
-    FragmentProfileBinding binding;
-    DatabaseReference reference;
-    FirebaseUser firebaseUser;
+    private FragmentProfileBinding binding;
+    private DatabaseReference reference;
+    private FirebaseUser firebaseUser;
+    private Context context;
+
+    //todo 72
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +68,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        context = view.getContext();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users_chat_app").child(firebaseUser.getUid());
         reference.addValueEventListener(new ValueEventListener() {
@@ -61,5 +88,104 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+
+        //todo 73
+        storageReference = FirebaseStorage.getInstance().getReference("uploads_chat_app");
+
+        binding.profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImage();
+            }
+        });
+    }
+
+    //todo 74
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+    //todo 75
+    private String getFileExtention(Uri uri){
+        ContentResolver contentResolver = context.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    //todo 76
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(context);
+        pd.setMessage("Uploading");
+        pd.show();
+
+        if(imageUri != null){
+            Toast.makeText(context, getFileExtention(imageUri), Toast.LENGTH_SHORT).show();
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()+"-"+getFileExtention(imageUri));
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users_chat_app").child(firebaseUser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+
+                        pd.dismiss();
+                    } else {
+                        Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        } else {
+            Toast.makeText(context, "Belum ada gambar yang dipilih!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //todo 77
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            Toast.makeText(context, getFileExtention(imageUri), Toast.LENGTH_SHORT).show();
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(context, "Upload in progress", Toast.LENGTH_SHORT).show();
+            } else {
+                uploadImage();
+            }
+        }
+        //todo 78 ubah setingan storage di firebase
+        //rules_version = '2';
+        //service firebase.storage {
+        //  match /b/{bucket}/o {
+        //    match /{allPaths=**} {
+        //      allow read, write: if true;
+        //    }
+        //  }
+        //}
     }
 }
